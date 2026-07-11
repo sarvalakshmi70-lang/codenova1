c9225d2462700f8e8e443306be0f8f87a6d50b97
sarva <sarvalakshmi70@gmail.com>
2026-07-11 21:17:42 +0530

    basic of javascript

---
+++ day2.js
@@ -0,0 +1,68 @@
+// variable declaration
+var name = "John Doe";
+let age = 30;
+const country = "USA";
+//using var
+var a=10
+var b=20
+{
+var c= a+b
+}
+console.log(c) 
+//using let
+let d = 30;
+let e = 40;
+{
+let f = d + e;
+console.log(f);
+}
+//using const
+const price=100
+
+//function calling
+function samsung()
+ {
+    console.log("this is a samsung phone")
+ }
+ samsung()
+
+ function iphone()
+ {
+    console.log("this is an iphone")
+ }
+ iphone()
+
+ function redmi()
+ {
+    console.log("this is a redmi phone")
+ }
+ redmi()
+ //function addition
+var a=10  
+var b=20
+function add()
+{
+    var c=a+b
+    console.log(c)
+}
+add()
+ 
+//function concatination
+var favouriteActor = "vijay"
+var favoritePlayer = "dhoni"
+var favouriteMovie = "vikram"
+function myfavourite()
+{
+    console.log("my favourite actor is " + favouriteActor)
+    console.log("my favourite player is " + favoritePlayer)
+    console.log("my favourite movie is " + favouriteMovie)
+}
+myfavourite()
+
+//string
+var fruit = "apple"
+var price = 100
+var count=5
+var total = price * count
+console.log("fruit name is " + fruit)
+console.log(total)
